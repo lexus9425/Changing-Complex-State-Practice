bea1ea2075fc4dc7fd5c2b3df60003d323690b73
Alexis Ojeda <lexus9425@gmail.com>
2022-08-30 16:41:51 +0000

    Final Commit
    - Render First Name, Last Name, and Email provided by user.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,30 @@ function App() {
     email: ""
   });
 
+  function handleChange(event) {
+    const {value, name} = event.target;
+    
+    setContact((prevValue) => {
+      if(name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        }
+      }
+    });
+  }
+
   return (
     <div className="container">
       <h1>
@@ -14,9 +38,27 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input 
+          onChange={handleChange} 
+          type="text" 
+          name="fName" 
+          placeholder="First Name" 
+          value={contact.fName}
+        />
+        <input 
+          onChange={handleChange} 
+          type="text" 
+          name="lName" 
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input 
+          onChange={handleChange} 
+          type="text" 
+          name="email" 
+          placeholder="Email"
+          value={contact.email}
+        />
         <button>Submit</button>
       </form>
     </div>
